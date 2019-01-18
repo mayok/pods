922940ab98f3ad42850beb5a21590286aec98df5
Naohiro IWAMOTO <naodai24@gmail.com>
2019-01-19 01:04:04 +0900

    fetchContents の戻り値を修正した

--- src/provider/api.ts
+++ src/provider/api.ts
@@ -6,6 +6,9 @@ import { Channel } from "../interfaces";
 interface IList {
   channels: string[];
 }
+interface IContents {
+  [x: string]: Channel;
+}
 export const fetchList = async (): Promise<string[]> => {
   const response = await fetch(`${config.host}:${config.port}/podcast/`);
   const list: IList = await response.json();
@@ -14,7 +17,7 @@ export const fetchList = async (): Promise<string[]> => {
 };
 
 // fetch channel contents
-export const fetchContents = async (channel: string): Promise<Channel> => {
+export const fetchContents = async (channel: string): Promise<IContents> => {
   // todo: think when server returns error
   const response = await fetch(`${config.host}:${config.port}/podcast/${channel}/`);
   const xml = await response.text();
@@ -24,12 +27,14 @@ export const fetchContents = async (channel: string): Promise<Channel> => {
   const json = JSON.parse(json_string);
 
   return {
-    title: json.rss.channel.title._text,
-    contents: json.rss.channel.item.map((e: any) => ({
-      title: e.title._text,
-      url: e.guid._text,
-      type: e.enclosure._attributes.type,
-      date: e.pubDate
-    }))
+    [channel]: {
+      title: json.rss.channel.title._text,
+      contents: json.rss.channel.item.map((e: any) => ({
+        title: e.title._text,
+        url: e.guid._text,
+        type: e.enclosure._attributes.type,
+        date: e.pubDate
+      }))
+    }
   };
 };
