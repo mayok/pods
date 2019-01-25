93bff27416fbe50affc77f2bb62fe74684366487
Naohiro Iwamoto <naodai24@gmail.com>
2019-01-25 13:02:03 +0900

    ざっくり media component 書いた

--- src/components/media.tsx
+++ src/components/media.tsx
@@ -10,9 +10,22 @@ const Media = React.memo(({ media }: Props) => {
   useEffect(() => {
     if (media.type) {
       // get media element, video or audio
-      // if media == video:
-      //   media.requestpip()
-      // media.play()
+      const _media = document.querySelector('#media') as
+        | HTMLVideoElement
+        | HTMLAudioElement
+
+      // if type equals video, use play in pip mode
+      if (media.type.split('/')[0] === 'video') {
+        ;(_media as any).requestPictureInPicture()
+      }
+
+      // play
+      _media.play()
+
+      // register EventListener
+      _media.addEventListener('leavepictureinpicture', () => {
+        // do something
+      })
     }
   })
 
@@ -22,20 +35,33 @@ const Media = React.memo(({ media }: Props) => {
   }
 
   return (
-    <>
-      {media.type === 'video' ? (
-        <video src={media.url} />
+    <Player>
+      {media.type.split('/')[0] === 'video' ? (
+        <video id="media" src={media.url} />
       ) : (
-        <audio src={media.url} />
+        <audio id="media" src={media.url} />
       )}
 
-      <Controls />
-    </>
+      <Controls>
+        <Play className="play" />
+        <Rewind className="rewind" />
+        <Forward className="rewind" />
+      </Controls>
+    </Player>
   )
 })
 
 export default Media
 
+const Player = styled.div`
+  width: 100vw;
+  height: 100vw;
+`
+
 const Controls = styled.div`
   width: 80vw;
 `
+
+const Play = styled.button``
+const Rewind = styled.button``
+const Forward = styled.button``
