46039ec0062e7d20b7be761fbc7f0328160c334e
Naohiro IWAMOTO <naodai24@gmail.com>
2019-01-26 00:20:16 +0900

    any だからだめ ?

--- src/utils/video.ts
+++ src/utils/video.ts
@@ -1,10 +1,9 @@
 export function requestPiP(setMedia: Function) {
   const video = document.querySelector('#video') as HTMLVideoElement
 
-  video.addEventListener('loadeddata', () => {
-    ;(video as any).requestPictureInPicture().catch((e: string) => {
-      console.log(e)
-    })
+  video.addEventListener('loadedmetadata', () => {
+    // @ts-ignore: video does not have requestPictureInPicture API yet
+    video.requestPictureInPicture()
   })
 
   video.addEventListener('leavepictureinpicture', () => {
